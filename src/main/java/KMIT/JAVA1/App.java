c7bcdedaa65bb308748fad834f212ce91c893649
venuvarma <gvgvarma.1234@gmail.com>
2025-12-04 09:45:28 +0530

    ls

---
+++ src/main/java/KMIT/JAVA1/App.java
@@ -0,0 +1,10 @@
+package KMIT.JAVA1;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! this is java maven project");
+    }
+}
